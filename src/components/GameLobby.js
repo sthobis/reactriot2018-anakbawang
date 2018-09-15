01f07291e67cb3fb9b3f9f806cc1d869e1259da5
Aji Nugraha Santosa Kasmaji <ajiajikasmaji@gmail.com>
2018-09-15 17:49:20 +0900

    feat: GameLobby keyboard event listener

--- src/components/GameLobby.js
+++ src/components/GameLobby.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 import Youtube from "react-youtube";
 
+const KEYS_CODE = {
+  ENTER: 13,
+  ESC: 27,
+  SPACE: 32,
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+};
+
 const songList = [
   {
     title: "So Eun Lee - Loving You",
@@ -22,40 +32,71 @@ const songList = [
 
 class GameLobby extends Component {
   state = {
-    selectedSong: songList[0]
+    selectedSongIndex: 0
   };
 
-  handleSelectSong = selectedSong => {
-    this.setState({ selectedSong });
+  handleKeyDown = e => {
+    const { handleChangeSong } = this.props;
+    const { selectedSongIndex } = this.state;
+
+    switch (e.which) {
+      case KEYS_CODE.UP:
+        console.log("up");
+        this.setState(state => ({
+          selectedSongIndex:
+            (state.selectedSongIndex + songList.length - 1) % songList.length
+        }));
+        break;
+      case KEYS_CODE.DOWN:
+        console.log("down");
+        this.setState(state => ({
+          selectedSongIndex: (state.selectedSongIndex + 1) % songList.length
+        }));
+        break;
+      case KEYS_CODE.ENTER:
+        console.log(songList[this.state.selectedSongIndex]);
+        this.props.handleChangeSong(songList[this.state.selectedSongIndex]);
+        break;
+      default:
+        break;
+    }
+  };
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  handleSelectSong = selectedSongIndex => {
+    this.setState({ selectedSongIndex });
   };
 
   render() {
     const { handleChangeSong } = this.props;
-    const { selectedSong } = this.state;
+    const { selectedSongIndex } = this.state;
 
     return (
       <div>
         Game Lobby!
         <div>
-          {songList.map(song => (
-            <div>
-              <button onClick={() => this.handleSelectSong(song)}>
+          {songList.map((song, index) => (
+            <div key={songList[index].youtubeId}>
+              <button onClick={() => handleChangeSong(songList[index])}>
                 {song.title} ({song.bpm} bpm)
               </button>
             </div>
           ))}
         </div>
-        <div>
-          <button onClick={() => handleChangeSong(selectedSong)}>play!</button>
-        </div>
         <Youtube
-          videoId={selectedSong.youtubeId}
+          videoId={songList[selectedSongIndex].youtubeId}
           opts={{
             height: "390",
             width: "640",
             playerVars: {
               autoplay: 1,
-              start: selectedSong.previewStartAt
+              start: songList[selectedSongIndex].previewStartAt
             }
           }}
         />
